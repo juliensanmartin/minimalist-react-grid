51a27af6d711e15bc7337e77e638eca1c69b8552
Julien Sanmartin <sanmartinjulien@gmail.com>
2018-04-05 09:09:43 -0700

    add grid example with images

--- src/docs/index.js
+++ src/docs/index.js
@@ -57,6 +57,44 @@ function Demo() {
           </div>
         </GreenGrid>
       </OrangeDiv>
+      <h1>Demo Grid</h1>
+      <GreenGrid spacing={16}>
+        <img 
+          width="100%" 
+          height="100%"
+          src="https://cdn.pixabay.com/photo/2016/11/23/01/18/adorable-1851661_1280.jpg" 
+          size={6}/>
+        <img 
+          width="100%" 
+          height="100%"
+          src="https://cdn.pixabay.com/photo/2017/12/26/16/09/lion-3040797_1280.jpg" 
+          size={4}/>
+        <img 
+          width="100%" 
+          height="100%"
+          src="https://cdn.pixabay.com/photo/2017/11/06/09/53/animal-2923186_1280.jpg" 
+          size={2}/>
+        <img 
+          width="100%" 
+          height="100%"
+          size={12}
+          src="https://cdn.pixabay.com/photo/2018/01/22/14/13/animal-3099035_1280.jpg"/>
+        <img 
+          width="100%" 
+          height="100%"
+          src="https://c.pxhere.com/photos/e4/81/hedgehog_animal_baby_cute_small_pet-932170.jpg!d" 
+          size={4}/>
+        <img 
+          width="100%" 
+          height="100%"
+          src="https://cdn.pixabay.com/photo/2018/03/23/07/21/swan-3252871_1280.jpg" 
+          size={4}/>
+        <img 
+          width="100%" 
+          height="100%"
+          src="https://cdn.pixabay.com/photo/2017/04/30/18/33/cat-2273598_1280.jpg" 
+          size={4}/>
+      </GreenGrid>
     </div>
   );
 }
